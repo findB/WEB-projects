4fedc5fe1b33e645ccac5ee8c892d50f318a6631
Vygandas Pliasas <vygandasp@gmail.com>
2018-06-04 22:13:29 +0300

    Update index.js

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,7 +1,11 @@
+// Neleisti vykdyti JS kodo toliau pasitaikius klaidai
 'use strict';
 
+// Daugiau informacijos apie "Hot Module Replacement"
+// https://webpack.js.org/concepts/hot-module-replacement/
 if (module.hot) {
     module.hot.accept();
 }
 
+// Įtraukiami stiliai
 import '../styles/common.scss';
